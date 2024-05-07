183747ddf873829f4c798d9b08f95ae35f901aa8
david30-maker <davidokpah396@gmail.com>
2024-05-07 09:35:39 +0100

    handle storage of data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,25 @@ class PostController extends Controller
     public function create() {
         return view('posts.create');
     }
+
+    // Store Post
+    public function store(Request $request) {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            ]);
+
+            $post = new Post;
+
+            $file_name =time() . '.' . request()->iage->getClientOriginalExtension();
+            request()->image->move(public_path('image'), $file_name);
+
+            $post->title = $request->title;
+            $post->description = $request->description;
+            $post->image = $file_name;
+
+            $post->save();
+            return redirect()->route('posts.index')->with('success', "Post created successfuly.");
+    }
 }
