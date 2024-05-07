a45c84fca1013804309bd24c1448d8589f64d265
david30-maker <davidokpah396@gmail.com>
2024-05-07 10:37:52 +0100

    update index method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,38 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use APP\Models\Post;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index() {
-        $post = Post::orderBy('created_at', 'desc')->get();
-        return view('posts.index', ['post'=>$post]);
-    }
-
+  public function index() {
+      $posts = Post::orderBy('created_at', 'desc')->get();
+      return view('posts.index', ['posts' => $posts]);
+  }
     // Create Post
     public function create() {
         return view('posts.create');
     }
 
-    // Store Post
-    public function store(Request $request) {
-        $request->validate([
-            'title' => 'required',
-            'description' => 'required',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            ]);
-
-            $post = new Post;
-
-            $file_name =time() . '.' . request()->iage->getClientOriginalExtension();
-            request()->image->move(public_path('image'), $file_name);
-
-            $post->title = $request->title;
-            $post->description = $request->description;
-            $post->image = $file_name;
-
-            $post->save();
-            return redirect()->route('posts.index')->with('success', "Post created successfuly.");
-    }
+    // Store post
+public function store(Request $request) {
+    // validations
+    $request->validate([
+      'title' => 'required',
+      'description' => 'required',
+      'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    ]);
+  
+    $post = new Post;
+  
+    $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+    request()->image->move(public_path('images'), $file_name);
+  
+    $post->title = $request->title;
+    $post->description = $request->description;
+    $post->image = $file_name;
+  
+    $post->save();
+    return redirect()->route('posts.index')->with('success', 'Post created successfully.');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::resource('posts', PostController::class)->names([
     'index' => 'posts.index',
     'create' => 'posts.create',
     'store' => 'posts.store',
     'show' => 'posts.show',
-    // 'edit' => 'posts.edit',
-    // 'update' => 'posts.update',
-    // 'destroy' => 'posts.destroy',
 ]);
 
